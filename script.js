e82c5c419ea904eee4c6fb7eac983cc7f77ff515
Christian Marquez <christianmarquez41@gmail.com>
2020-09-20 19:55:11 -0400

    added another call to get the city info from the lat and lon obtained from first call

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ var formColumnEl = $("#form-space");
 
 var buttonsEl = $("#button-area");
 // city name taken from user
-var cityName
+var cityName;
+// coordinates of the city
+var cityLon;
+var cityLat;
 
 // adding buttons with the name of the city being searched
 formEl.submit(function (event) {
@@ -26,30 +29,46 @@ formEl.submit(function (event) {
 })
 
 // my API key
-var APIKey = "9d222a95ad8abb11aa3604f18c12a818"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=&appid=" + APIKey;
+var APIKey = "fe4bb57179968f6fb5c640fef4d924e0"
+// url to get city's coordinates
+var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=&appid=" + APIKey;
+var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=&appid=" + APIKey;
 
 
 
 
 $(buttonsEl).on("click", function () {
-    queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=" + cityName + "&appid=" + APIKey;
+    queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
     // running AJAX call to the OpenWeaterMap API
     $.ajax({
-        url: queryURL,
+        url: queryURL1,
         method: "GET"
     })
 
         .then(function (response) {
 
-            console.log(queryURL);
+
             console.log(response);
+            cityLon = response.coord.lon;
+            cityLat = response.coord.lat;
 
+            console.log(cityLon);
+            console.log(cityLat);
 
+            queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=&appid=" + APIKey;
 
 
         });
+
+    $.ajax({
+        ulr: queryURL2,
+        method: "GET"
+    })
+
+        .then(function (response) {
+
+            console.log(response);
+
+        })
 
 });
