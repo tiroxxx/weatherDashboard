ef98d33e3ddf4d2e28e5b70d1f50d37214dd9505
Christian Marquez <christianmarquez41@gmail.com>
2020-09-22 17:57:49 -0400

    stored and retrieved the cities from local storage

--- script.js
+++ script.js
@@ -1,101 +1,119 @@
-// hooking up my form elements
-var formEl = $("#formy");
-var formInput = $("#form-input");
-var formButton = $("#form-button");
-// hooking into the col where the form is
-var formColumnEl = $("#form-space");
+$(document).ready(function () {
 
-var buttonsEl = $("#button-area");
-// city name taken from user
-var cityName = [];
-// coordinates of the city
-var cityLon;
-var cityLat;
-// initialize page
-init();
 
-function init() {
-    var temp = JSON.parse(localStorage.getItem("city-name"));
-    // check if theres anything stored
-    if (storedInfo !== null) {
-        cityName = temp;
+    // hooking up my form elements
+    var formEl = $("#formy");
+    var formInput = $("#form-input");
+    var formButton = $("#form-button");
+    // hooking into the col where the form is
+    var formColumnEl = $("#form-space");
+
+    var buttonsEl = $("#button-area");
+    // city name taken from user
+    var cityName = [];
+    // coordinates of the city
+    var cityLon;
+    var cityLat;
+    // initialize page
+    init();
+
+    function init() {
+        // get the cities stored in local storage
+        var temp = JSON.parse(localStorage.getItem("city-name"));
+        // check if theres anything stored
+        if (temp !== null) {
+            cityName = temp;
+        }
+
+        var length = cityName.length;
+        // if the length of the array is bigger than 7, make it 7
+        if (length > 7) {
+            length = 7;
+        }
+        // loop through the cities we got from local storage
+        for (var i = 0; i < length; i++) {
+            // create a button and add attributes for each city
+            var newButton = $("<button>");
+            newButton.attr("type", "button");
+            newButton.text(cityName[i]);
+            newButton.addClass("btn");
+            newButton.addClass("button");
+            newButton.addClass("btn-light");
+            newButton.attr("data-city", cityName[i]);
+            // append the button to the page
+            buttonsEl.append(newButton)
+        };
     }
 
-    var newButton = $("<button>");
-    newButton.attr("type", "button");
-    newButton.text(storedInfo);
-    newButton.addClass("btn");
-    newButton.addClass("button");
-    newButton.addClass("btn-light");
-    newButton.attr("data-city", storedInfo);
+    // adding buttons with the name of the city being searched
+    formEl.submit(function (event) {
+        event.preventDefault();
+        // city name taken from user
+        cityName.unshift(formInput.val());
+        // store that city in local storage
+        localStorage.setItem("city-name", JSON.stringify(cityName));
 
-    buttonsEl.append(newButton);
+        // var newButton = $("<button>");
+        // newButton.attr("type", "button");
+        // newButton.text(cityName);
+        // newButton.addClass("btn");
+        // newButton.addClass("button");
+        // newButton.addClass("btn-light");
+        // newButton.attr("data-city", cityName);
 
-}
-// adding buttons with the name of the city being searched
-formEl.submit(function (event) {
-    event.preventDefault();
-    // city name taken from user
-    cityName.unshift(formInput.val());
+        // buttonsEl.append(newButton);
 
-    localStorage.setItem("city-name", cityName);
-    // var newButton = $("<button>");
-    // newButton.attr("type", "button");
-    // newButton.text(cityName);
-    // newButton.addClass("btn");
-    // newButton.addClass("button");
-    // newButton.addClass("btn-light");
-    // newButton.attr("data-city", cityName);
+        currentWeather(cityName);
 
-    // buttonsEl.append(newButton);
+    });
 
-    currentWeather(cityName);
+    // my API key
+    var APIKey = "fe4bb57179968f6fb5c640fef4d924e0"
+
+    function currentWeather(city) {
+
+        var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+        // running AJAX call to the OpenWeaterMap API
+        $.ajax({
+            url: queryURL1,
+            method: "GET"
+        })
+            // once the info is retrieved from api run this
+            .then(function (response) {
+
+                console.log(response);
+                cityLon = response.coord.lon;
+                cityLat = response.coord.lat;
+
+                console.log(cityLon);
+                console.log(cityLat);
+
+                // CALL function to get forecast and pass in lat and lon
+                getForecast(cityLat, cityLon);
+
+            });
+
+
+
+    }
+
+    function getForecast(lat, lon) {
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&appid=" + APIKey;
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        })
+
+            .then(function (response) {
+
+                console.log(response);
+
+            })
+
+    }
+
 
 });
 
-// my API key
-var APIKey = "fe4bb57179968f6fb5c640fef4d924e0"
 
 
-function currentWeather(city) {
-
-    var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    // running AJAX call to the OpenWeaterMap API
-    $.ajax({
-        url: queryURL1,
-        method: "GET"
-    })
-
-        .then(function (response) {
-
-
-            console.log(response);
-            cityLon = response.coord.lon;
-            cityLat = response.coord.lat;
-
-            console.log(cityLon);
-            console.log(cityLat);
-
-            // CALL function to get forecast and pass in lat and lon
-            getForecast(cityLat, cityLon);
-
-        });
-
-
-
-}
-
-function getForecast(lat, lon) {
-    var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&appid=" + APIKey;
-    $.ajax({
-        url: queryURL2,
-        method: "GET"
-    })
-
-        .then(function (response) {
-
-            console.log(response);
-
-        })
-
-}
